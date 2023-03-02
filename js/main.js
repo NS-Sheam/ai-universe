7260bf7fa3a136ebe8bdd6b4452e126926a2bb1d
NS-Sheam <123sheamfeni@gmail.com>
2023-03-02 23:46:20 +0600

    Adding ui features done

--- js/main.js
+++ js/main.js
@@ -1,20 +1,30 @@
+// Fetching all data 
 const loadData = async() =>{
     const url = 'https://openapi.programming-hero.com/api/ai/tools';
     const res = await fetch(url);
     const data = await res.json();
     displayData(data.data.tools);
 };
+
+// Displaying All data by card 
 const displayData = allData =>{
     const cardContainer = document.getElementById('card-container');
+    allData = allData.slice(0, 6);
     allData.forEach(data => {
         const cardDiv = document.createElement('div');
         cardDiv.classList.add('card', 'w-96', 'bg-base-100', 'shadow-xl', 'mx-auto', 'mb-5' , 'border-1');
         console.log(data);
+        // Showing data in card 
         cardDiv.innerHTML = `
                 <figure><img src="${data.image}"/></figure>
                 <div class="card-body">
                   <h2 class="card-title font-bold">Features</h2>
-                  <p>${data.features}</p>
+                  <ol id="feature-ol">
+                  <li>1. ${data.features[0] ? data.features[0] : 'No more details'}</li>
+                  <li>2. ${data.features[1] ? data.features[1] : 'No more details'}</li>
+                  <li>3. ${data.features[2] ? data.features[2] : 'No more details'}</li>
+                  <li>4. ${data.features[3] ? data.features[3] : 'No more details'}</li>
+                  </ol>
                   <hr class="border-1 border-black">
                   <h2 class="font-bold text-xl">${data.name}</h2>
                   <div class="flex justify-between items-center">
@@ -24,6 +34,13 @@ const displayData = allData =>{
                 </div>
         `;
         cardContainer.appendChild(cardDiv);
+        // const featureListContainer = document.getElementById('feature-ol');
+        // const featureList = document.createElement('li');
+        //     featureList.innerText = data.features;
+        //     featureListContainer.appendChild(featureList);
+        // // data.features.forEach(feature => {
+            
+        // // });
     });
 };
 loadData();
