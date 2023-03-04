1d2f4cda51b3f9d02c0c50f9e537da2a44f96248
NS-Sheam <123sheamfeni@gmail.com>
2023-03-04 10:08:37 +0600

    Modal left side functionality added

--- js/main.js
+++ js/main.js
@@ -72,7 +72,6 @@ const loadModalData = async (id) => {
     displayModalData(data.data);
 };
 const displayModalData = (data) => {
-    console.log(data);
     const modalLeftDescription = document.getElementById('modal-left-description');
     const pricingBox1 = document.getElementById('pricing-box-1');
     const pricingBox2 = document.getElementById('pricing-box-2');
@@ -80,13 +79,27 @@ const displayModalData = (data) => {
     const featureListContainer = document.getElementById('feature-list-container');
     const integrationListContainer = document.getElementById('integration-list-container');
     const modalClassImg = document.getElementById('modal-class-img');
-    console.log();
+    console.log(data);
     modalLeftDescription.innerText = data.description ? data.description : "No data found";
-    pricingBox1.innerText = data.pricing[0] && data.pricing[0].price !== '0' && data.pricing[0].price !== "No cost" ? data.pricing[0].price + "\n" + data.pricing[0].plan : "Free Of Cost/Basic";
-    pricingBox2.innerText = data.pricing[1] && data.pricing[1].price !== '0' && data.pricing[1].price !== "No cost" ? data.pricing[1].price + "\n" + data.pricing[1].plan : "Free Of Cost/Basic";
-    pricingBox3.innerText = data.pricing[2] && data.pricing[2].price !== '0' && data.pricing[2].price !== "No cost" ? data.pricing[2].price + "\n" + data.pricing[2].plan : "Free Of Cost/Basic";
+    // Pricing box with validation
+    pricingBox1.innerText = data.pricing && data.pricing[0] && data.pricing[0].price !== '0' && data.pricing[0].price !== "No cost" ? data.pricing[0].price + "\n" + data.pricing[0].plan : "Free Of Cost/Basic";
+    pricingBox2.innerText = data.pricing &&  data.pricing[1] && data.pricing[1].price !== '0' && data.pricing[1].price !== "No cost" ? data.pricing[1].price + "\n" + data.pricing[1].plan : "Free Of Cost/Basic";
+    pricingBox3.innerText = data.pricing && data.pricing[2] && data.pricing[2].price !== '0' && data.pricing[2].price !== "No cost" ? data.pricing[2].price + "\n" + data.pricing[2].plan : "Free Of Cost/Basic";
+    featureListContainer.innerHTML = ' ';
+    // Adding feature list item in modal 
+    for (const key in data.features) {
+        const featureList = document.createElement('li');
+        featureList.innerText = data.features[key].feature_name ? data.features[key].feature_name : 'No details found';
+        featureListContainer.appendChild(featureList);
+    };
+    integrationListContainer.innerHTML = ' ';
+    data.integrations ? data.integrations.forEach(element => {
+        const integrationList = document.createElement('li');
+        integrationList.innerText = element ? element : 'No data found';
+        integrationListContainer.appendChild(integrationList);
+    }) : integrationListContainer.innerHTML = `<li> No data found</li>`;
 };
-loadModalData()
+loadModalData();
 
 
 // {/* <li>1. ${data.features[0] ? data.features[0] : 'No more details'}</li>
